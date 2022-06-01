137e42bc3a2109cb079061ddeab3152692453cc1
Haaris <89451392+haaris272k@users.noreply.github.com>
2022-06-01 17:56:10 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,89 @@
+# Pro-Buddy-Bot
+A multifunctional telegram bot that can provide the user with necessary updates and help him increase his productivity.
+
+# Usage
+
+ *Getting Started* - The bot has already been deployed and is currently in use. Search for 'ProBuddyBot' in telegram and start using it.
+ 
+![bot](https://user-images.githubusercontent.com/89451392/161108890-523c9fe6-e41c-46ea-adbf-70789ff2ba0f.png)
+ 
+ 
+# Features
+
+  - Provides with the necessary updates and increases productivity of the user.
+
+  - Responsive, easy to use and has around 10 functionalities -
+
+        sends the trending news of the day
+
+        gives weather update
+
+        gives the live score of some cricket match
+
+        ask bot to tell u a joke
+
+        ask bot to send u a meme
+
+        you can get a random short poem 
+
+        read random quotes
+
+        get to know some random facts
+
+        get to know which western and bollywood songs are topping the charts
+        
+ - If a user enters the wrong command then bot would send an interactive message with an appealing media ( media would be some cute random pic of an animal(s) )
+
+    ![wrong command](https://user-images.githubusercontent.com/89451392/161107305-61f3ef36-9a71-455e-b588-ca81f44e266a.png)
+
+# Commands
+              
+    /start - to know about the bot.
+    
+   ![start](https://user-images.githubusercontent.com/89451392/161107687-e7ea9587-2a50-46a9-81c7-60e420fc058d.png)
+
+    /hi - to greet the bot.
+    
+   ![hi](https://user-images.githubusercontent.com/89451392/161107754-5f41e4c9-bbb7-43fc-9c9b-977c498f4097.png)
+
+    /nu - to view the trending news of the day.
+    
+   ![news](https://user-images.githubusercontent.com/89451392/161107770-da2a9377-3dbd-4e25-b294-8ee5f005ba00.png)
+
+    /wu - to view the live weather update.
+    
+   ![wu](https://user-images.githubusercontent.com/89451392/161107780-f414b812-ac59-4f3f-8e40-66c021965a9b.png)
+
+    /cu - to view the live cricket score.
+    
+   ![cricket](https://user-images.githubusercontent.com/89451392/161107786-b79bf3d3-f10c-499b-aa92-55c4509da318.png)
+
+    /joke - to get a random joke.
+    
+   ![joke](https://user-images.githubusercontent.com/89451392/161107805-580290f3-77ce-4d59-80f4-c0911b773cbb.png)
+
+    /meme - to get a random meme.
+    
+   ![meme](https://user-images.githubusercontent.com/89451392/161107810-597f3d97-cb9f-4afc-bdbd-27561bf32c45.png)
+
+    /poem - to get a random poem.
+    
+   ![poem](https://user-images.githubusercontent.com/89451392/161107839-2e759c48-f521-4cc2-8f28-7c80ee2e6ebd.png)
+
+    /quote - to get a random quote.
+    
+   ![quote](https://user-images.githubusercontent.com/89451392/161107855-a883b35a-43e3-43f7-b91d-97e18f66ea24.png)
+
+    /fact - to get a random fact.
+    
+   ![fact](https://user-images.githubusercontent.com/89451392/161107869-c5c462d3-0298-4df4-9766-824b2adda0b1.png)
+    
+    /topw - to see the top 10 trending western songs of the week.
+    
+   ![topw](https://user-images.githubusercontent.com/89451392/161107885-f5370c92-8ad0-4fe5-a00c-6298b9ab2044.png)
+
+    /topb-  to see the top 10 trending bollywood songs of the week.
+    
+   ![topb](https://user-images.githubusercontent.com/89451392/161107895-c89c809c-f3cc-4c28-93e4-01e659958404.png)
+
+
---
+++ bot.py
@@ -0,0 +1,305 @@
+from bs4 import BeautifulSoup
+from data import *
+import requests
+import random
+import time
+import telebot
+
+
+# creating an instance of the TeleBot class
+bot = telebot.TeleBot("")
+# bot = telebot.TeleBot("") For when u'll create a new vbot
+
+# welcome function
+@bot.message_handler(commands=["start"])
+def hello(message):
+    l1 = "Hello, My name is Harry. My official name is 'ProBuddyBot'."
+    l2 = "I'm a telegram bot that can be your best Buddy in need."
+    l3 = "I can provide you with necessary updates and help you increase your Productivity."
+    l4 = "Feel free to add me to your groups and let others know about me"
+    bot.send_message(
+        message.chat.id,
+        l1
+        + "\n"
+        + "\n"
+        + l2
+        + "\n"
+        + "\n"
+        + l3
+        + "\n"
+        + "\n"
+        + l4
+        + "\n"
+        + "\n"
+        + "Thank you 😇"+"\n"+"\n"+"Developed by @Haaris272k"
+    )
+
+
+@bot.message_handler(commands=["hello", "hi", "hey"])
+def hello(message):
+    username = str(message.chat.first_name)
+    greet = random.choice(interactive_greet_response)
+    bot.send_message(message.chat.id, f"Hey {username}, {greet}")
+
+
+# send trending news function
+@bot.message_handler(commands=["nu"])
+def trending_news(message):
+
+    ############## getting the relevant data which is to be send by the bot ##############
+
+    r = requests.get(trending_news_link)
+    soup = BeautifulSoup(r.text, "html.parser")
+    news = soup.find_all("h3", {"class": "trenz_news_head lh22 listing_story_title"})
+    trending_news = []
+    for i in news:
+        trending_news.append(i.get_text())
+    final_display = " "
+    for j in trending_news[0:5]:
+        final_display = final_display + "*" + " " + j + "\n" + "\n"
+
+    ######################################################################################
+
+    # sending the relevant data to the telegram user
+    interactive_tn_response = random.choice(interactive_news_response)
+    bot.send_message(message.chat.id, interactive_tn_response)
+    time.sleep(0.5)
+    bot.send_message(message.chat.id, final_display)
+
+
+# send weather update function
+@bot.message_handler(commands=["wu"])
+def weather_update(message):
+
+    ############## getting the relevant data which is to be send by the bot ##############
+
+    r = requests.get(weather_update_link, headers=headers)
+    soup = BeautifulSoup(r.text, "html.parser")
+
+    """location = soup.find(
+        "h1", {"class": "CurrentConditions--location--kyTeL"}
+    ).get_text()"""
+
+    temperature = soup.find(
+        "span", {"class": "CurrentConditions--tempValue--3a50n"}
+    ).get_text()
+    condition = soup.find(
+        "div", {"class": "CurrentConditions--phraseValue--2Z18W"}
+    ).get_text()
+    weather = (
+        # "Your location is"
+        # + " "
+        # + location
+        # + "\n"+
+        "The current temperature is"
+        + " "
+        + temperature
+        + "C"
+        + " \n"
+        + "It's kinda "
+        + condition
+        + " out there"
+    )
+
+    ######################################################################################
+
+    # sending the relevant data to the telegram user
+    bot.send_message(message.chat.id, weather)
+
+
+# send cricket update function
+@bot.message_handler(commands=["cu"])
+def cricket_update(message):
+
+    ############## getting the relevant data which is to be send by the bot ##############
+
+    try:
+        r = requests.get(cricket_update_link)
+        soup = BeautifulSoup(r.text, "html.parser")
+        team_1 = soup.find_all(class_="cb-ovr-flo cb-hmscg-tm-nm")[0].get_text()
+        team_2 = soup.find_all(class_="cb-ovr-flo cb-hmscg-tm-nm")[1].get_text()
+        t1_score = soup.find_all(class_="cb-ovr-flo")[8].get_text()
+        t2_score = soup.find_all(class_="cb-ovr-flo")[10].get_text()
+        if t2_score == "" or t2_score[0] == " ":
+            cricket_update = (
+                team_1
+                + " "
+                + ":"
+                + " "
+                + t1_score
+                + "\n"
+                + team_2
+                + " "
+                + ":"
+                + " "
+                + "Balling"
+            )
+        else:
+            cricket_update = (
+                team_1
+                + " "
+                + ":"
+                + " "
+                + t1_score
+                + "\n"
+                + team_2
+                + " "
+                + ":"
+                + " "
+                + t2_score
+            )
+
+        ######################################################################################
+
+        # sending the relevant data to the telegram user
+        bot.send_message(message.chat.id, cricket_update)
+    except:
+        bot.send_message(
+            message.chat.id, "Sorry, I am not able to get any update rn! 😢"
+        )
+
+
+# send joke function
+@bot.message_handler(commands=["joke"])
+def joke(message):
+
+    # getting username of the telegram user
+    username = str(message.chat.first_name)
+
+    # sending the relevant data to the telegram user
+    bot.send_message(
+        message.chat.id,
+        f"{username}, you are really gonna fall for my sense of humour 😎🤏",
+    )
+
+    time.sleep(1.5)
+    bot.send_message(message.chat.id, random.choice(jokes))
+
+
+# send meme function
+@bot.message_handler(commands=["meme"])
+def meme(message):
+    try:
+        # sending the relevant data to the telegram user
+        bot.send_photo(message.chat.id, random.choice(memes))
+    except:
+        print("Some url is not working!")
+        # just in case if there is some broken url it will ignore it and send the meme
+        bot.send_photo(message.chat.id, random.choice(memes))
+
+
+# send poem function
+@bot.message_handler(commands=["poem"])
+def poem(message):
+
+    # sending the relevant data to the telegram user
+    bot.send_message(message.chat.id, random.choice(poems))
+
+
+# send quote function
+@bot.message_handler(commands=["quote"])
+def quote(message):
+
+    # sending the relevant data to the telegram user
+    bot.send_message(
+        message.chat.id, "Quotes are the reason why i am such a great bot 🤠"
+    )
+    time.sleep(1)
+    bot.send_message(message.chat.id, random.choice(quotes))
+
+
+# sending a random fact functionality
+@bot.message_handler(commands=["fact"])
+def fact(message):
+
+    # sending the relevant data to the telegram user
+    bot.send_message(message.chat.id, "Here's a fact for you 😉")
+    time.sleep(1)
+    bot.send_message(message.chat.id, random.choice(facts))
+
+
+# showing top 10 western songs of the week
+@bot.message_handler(commands=["topw"])
+def top_western_songs(message):
+
+    ############## getting the relevant data which is to be send by the bot ##############
+
+    r = requests.get(western_songs_link, headers=headers)
+    soup = BeautifulSoup(r.text, "html.parser")
+    extracting = soup.find(
+        "div",
+        {
+            "class": "chart-results-list // lrv-u-padding-t-150 lrv-u-padding-t-050@mobile-max"
+        },
+    ).find_all("h3", {"id": "title-of-a-story"})
+    songs = []
+    for song in extracting:
+        songs.append(song.get_text().strip())
+    list_songs = songs[2:12]
+    top_10_western = " "
+    for i in range(len(list_songs)):
+        top_10_western = top_10_western + f"{i+1}" + " " + list_songs[i] + "\n" + "\n"
+
+    ######################################################################################
+
+    # sending the relevant data to the telegram user
+    bot.send_message(message.chat.id, "Looks like someone is in the mood to party 😈")
+    time.sleep(1)
+    bot.send_message(message.chat.id, "Here's the 10 western songs topping the chart 🎶")
+    time.sleep(0.5)
+    bot.send_message(message.chat.id, top_10_western)
+
+
+# showing top 10 bollywood songs of the week
+@bot.message_handler(commands=["topb"])
+def top_bollywood_songs(message):
+
+    ############## getting the relevant data which is to be send by the bot ##############
+
+    r = requests.get(bollywood_songs_link, headers=headers)
+    soup = BeautifulSoup(r.text, "html.parser")
+    extracting = soup.find_all("a", {"class": "u-color-js-gray"})
+    songs = []
+    for song in extracting:
+        songs.append(song.get_text().strip())
+    list_songs = songs[0:10]
+    top_10_bollywood = " "
+    for i in range(len(list_songs)):
+        top_10_bollywood = (
+            top_10_bollywood + f"{i+1}" + " " + list_songs[i] + "\n" + "\n"
+        )
+
+    ######################################################################################
+
+    # sending the relevant data to the telegram user
+    bot.send_message(
+        message.chat.id,
+        "Oh cool bollywood!, Let's see what's making buzz in the indian musical industry these days 😜",
+    )
+    time.sleep(2)
+    bot.send_message(message.chat.id, top_10_bollywood)
+
+
+# To send some media if a user enters a wrong command
+@bot.message_handler(func=lambda m: True)
+def repeat(message):
+
+    # randomly choosing a media to send it to the user when he enters a wrong command
+    selected_cute_media = random.choice(cute_media_collection)
+
+    # sending the media to the telegram user
+    bot.send_message(message.chat.id, "Ooopsie, You've entered the wrong command 😕")
+    time.sleep(1.5)
+    bot.send_message(
+        message.chat.id, "It's not a problem, let me get a smile on your face 😉"
+    )
+    time.sleep(1.3)
+
+    # checking if the selected media is a photo or a video
+    if selected_cute_media[-3:] == "gif":
+        bot.send_animation(message.chat.id, selected_cute_media)
+    else:
+        bot.send_photo(message.chat.id, selected_cute_media)
+
+
+bot.infinity_polling()
